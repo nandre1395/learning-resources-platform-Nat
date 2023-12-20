00a01008832591ef27a41a6c25bbe64ae5468f2e
AndreaB113 <116130652+nandre1395@users.noreply.github.com>
2023-12-20 00:32:11 -0500

    Formulario

---
+++ resources/js/Components/ResourceForm.vue
@@ -0,0 +1,30 @@
+<script setup>
+import axios from "axios";
+import {ref} from "vue"
+
+    let title = ref('');
+    let description = ref('');
+    let link = ref('');
+
+    function createResource(){
+       axios
+       .post("/api/resources",{
+        title: title.value,
+        description: description.value,
+        link: link.value,
+       })
+       .then((response)=>{
+        console.log(response);
+       })
+    }
+
+</script>
+
+<template>
+<div class="m-8">
+    <input type="text" v-model="title">
+    <input type="text" v-model="description">
+    <input type="text" v-model="link">
+    <button @click="createResource">Crear Recurso</button>
+</div>
+</template>
